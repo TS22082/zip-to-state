27e3aecef42dc1266cb0441bbd73b17759fd9f34
TS22082 <ts22082@gmail.com>
2020-11-13 09:35:55 -0800

    allow all outside sources to make requests

---
+++ README.md
@@ -0,0 +1,10 @@
+# State from zip
+
+An api to get the 2 char state code from a zip code
+
+Example:
+`http://localhost:5000/check/32004`
+Returns "FL"
+
+`http://localhost:5000/check/94610`
+Returns "CA"
--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const app = express()
 const PORT = process.env.PORT || 5000
 var cors = require('cors')
 
+//configure middleware
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+//Routes
 app.use("/", require("./routes/api-routes"))
-
+//Server
 app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`))
